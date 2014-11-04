c38c3e5c57cc3206c4b9fbe0e4df83b28529e162
alishalisha <alisha.ramos@voxmedia.com>
2014-11-03 19:12:27 -0500

    I am seeing things yay

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,17 @@
 //= require_tree .
+
+var apiKey = 'OhYCz7P9q7cXNoMODeV8mHvkMFfEGzBiG4C8pP3C';
+var dataRequest = 'https://api.data.gov/gsa/auctions?api_key=' + apiKey + '&format=JSON&LocationST=MA&callback=?';
+
+
+$('h1').on('click', function(){
+  $.getJSON(dataRequest, function(data) {
+    console.log(data); //uncomment this for debug
+  //   //alert (data.item1+" "+data.item2+" "+data.item3); //further debug
+  //   //$('#showdata').html("<p>item1="+data.item1+" item2="+data.item2+" item3="+data.item3+"</p>");
+  });
+});
+
+// $('h1').on('click', function() {
+//   alert("hey");
+// })
